730934506459f4529c7e6e0d0cce3f67dc943500
Jakk Wong <jakk.wong@gmail.com>
2012-08-02 02:59:30 -0700

    Place modal in center

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,14 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready( function() {
+  $('#myModal').on('show', function () { 
+    $('#myModal').css ({ 
+                         'margin-top': function() {  
+                            return -($(this).height() / 2); 
+                         } 
+                      }); 
+  }); 
+}); 
+
