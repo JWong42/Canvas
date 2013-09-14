2d3f89a81da6d2c2316c07ff7017b46f53984511
Jakk Wong <jakk.wong@gmail.com>
2013-09-13 19:40:07 -0700

    Make realtime feeds available to relevent pages.

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -2,11 +2,12 @@ module FeedsHelper
 
   def get_notifications(user)
     @user = user
-    associations = @user.associations
-    canvases_id = associations.map(&:canvas_id)
-    @notifications = Feed.where(canvas_id: canvases_id).where.not(user_id: @user.id).order("created_at DESC")
-    # another way same query as above
-    # @notifications = Feed.where("canvas_id IN (?) AND user_id != ?", canvases_id, @user.id).order("created_at DESC")
+    unless @user.nil?
+      associations = @user.associations
+      canvases_id = associations.map(&:canvas_id)
+      @notifications = Feed.where(canvas_id: canvases_id).where.not(user_id: @user.id).order("created_at DESC")
+      # another way same query as above
+      # @notifications = Feed.where("canvas_id IN (?) AND user_id != ?", canvases_id, @user.id).order("created_at DESC")
+    end
   end 
-
 end
