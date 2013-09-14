d44ff8bdd498c395daa1bad27c08e1e56a2db116
Jakk Wong <jakk.wong@gmail.com>
2013-09-13 19:53:15 -0700

    Clean up redis events controller.

--- app/controllers/redis_events_controller.rb
+++ app/controllers/redis_events_controller.rb
@@ -13,18 +13,6 @@ class RedisEventsController < ApplicationController
         data = JSON.parse(data)
         emails = data['emails']
         if emails.include?(current_user.email) 
-          #feed = data['notification']
-          #feed_id = feed['id']
-          #if current_user.unread_feed.nil?
-            #UnreadFeed.create!(user_id: current_user.id, count: 1, list: [feed_id])
-            #data['unread'] = true 
-          #else
-            #unread_feed = current_user.unread_feed
-            #unread_feed_count = unread_feed.count + 1
-            #unread_feed_list = unread_feed.list + [feed_id]
-            #unread_feed.update_attributes!(user_id: current_user.id, count: unread_feed_count, list: unread_feed_list)
-          #end 
-          data['test'] = current_user.last_name
           data = data.to_json
           sse.write(data, event: 'feeds')
         end 
@@ -32,10 +20,9 @@ class RedisEventsController < ApplicationController
     end
     render nothing: true 
   rescue IOError
-    puts "IOError.\n"
+    puts "IOError."
     # When the client disconnects, we'll get an IOError on write
   ensure 
-    puts "closed.\n"
     redis.quit
     sse.close
   end 
