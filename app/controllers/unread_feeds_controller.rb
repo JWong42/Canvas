2bb2ad0ae26b09f07cd1babd187fcdc776fb9955
Jakk Wong <jakk.wong@gmail.com>
2013-09-13 19:41:26 -0700

    Change syntax.

--- app/controllers/unread_feeds_controller.rb
+++ app/controllers/unread_feeds_controller.rb
@@ -1,7 +1,7 @@
 class UnreadFeedsController < ApplicationController
   def update
     id = params[:id]
-    unread_feed = UnreadFeed.where(user_id: id)[0]
+    unread_feed = UnreadFeed.where(user_id: id).first
     unread_feed.update_attributes!(user_id: id, count: 0, list: [])
     render nothing: true
   end
