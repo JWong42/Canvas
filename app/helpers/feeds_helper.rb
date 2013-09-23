a45bd43a27fe28926ae081803745e2c050ba982d
Jakk Wong <jakk.wong@gmail.com>
2013-09-22 23:42:54 -0700

    Limit number of feeds that show up as notifications to 20.

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -5,9 +5,9 @@ module FeedsHelper
     unless @user.nil?
       associations = @user.associations
       canvases_id = associations.map(&:canvas_id)
-      @notifications = Feed.where(canvas_id: canvases_id).where.not(user_id: @user.id).order("created_at DESC")
+      @notifications = Feed.where(canvas_id: canvases_id).where.not(user_id: @user.id).order("created_at DESC").limit(20)
       # another way same query as above
-      # @notifications = Feed.where("canvas_id IN (?) AND user_id != ?", canvases_id, @user.id).order("created_at DESC")
+      # @notifications = Feed.where("canvas_id IN (?) AND user_id != ?", canvases_id, @user.id).order("created_at DESC").limit(20)
     end
   end 
 end
