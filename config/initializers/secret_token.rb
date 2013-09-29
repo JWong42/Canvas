0ec4f71b0c996d6ad67b36c36c12c3da8bd9dc7c
Jakk Wong <jakk.wong@gmail.com>
2013-09-28 18:18:50 -0700

    Add way to dynamically generate secret_key_base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,18 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BusinessModelCanvas::Application.config.secret_token = '4a905bc0183da25697eceb02392fc9a274c305410940e3098153709bc1fd17527b738dcf7ae855501d2f2901fce5cc8ba4eef332d2912986b5fe1b9c56bf5d29'
-BusinessModelCanvas::Application.config.secret_key_base = '4c553f91daa8002edaa6d7e25d2805d86ac7d45b32a194dbcc16ce21250ee73ebce74a4a3e0ca615cd278c75538735a283bf66f9a7a93046dc7e2a650aca4de7'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+BusinessModelCanvas::Application.config.secret_key_base = secure_token
