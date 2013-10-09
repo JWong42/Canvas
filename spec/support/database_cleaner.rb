1d6ed5622b70bd5c00c83b3c3142191b49a5df43
Jakk Wong <jakk.wong@gmail.com>
2013-10-08 22:38:19 -0700

    Modify database_cleaner configuration.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,23 +1,13 @@
 RSpec.configure do |config|
-
   config.before(:suite) do 
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do 
+    DatabaseCleaner.clean_with :truncation
     DatabaseCleaner.strategy = :transaction
   end
-
-  config.before(:each, :js => true) do 
-    DatabaseCleaner.strategy = :truncation
+  
+  config.around(:each) do |spec|
+    if spec.metadata[:js] || spec.metadata[:test_commit]
+      spec.run
+      DatabaseCleaner.clean_with :deletion
+    end
   end
-
-  config.before(:each) do 
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do 
-    DatabaseCleaner.clean
-  end
-
 end
